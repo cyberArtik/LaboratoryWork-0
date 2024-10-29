0a9620d9cf8f24a6082215b5d09d4b582361d100
Artiom <artemilico1337@gmail.com>
2024-10-29 17:06:29 +0200

    Comparison Area + PPI added

--- laboratory1/Task1/Display.ts
+++ laboratory1/Task1/Display.ts
@@ -14,14 +14,31 @@ export class Display {
     }
 
     // ---->
-    comparisonArea(display: Display): void {
+    comparisonArea(other: Display): void {
         console.log(
-            (this.width * this.height) > (display.width * display.height)
-                ? `Model: ${this.model} has a larger screen area of ${this.width * this.height}px² than model: ${display.model} with screen area of ${display.width * display.height}px².`
-                : this.width * this.height < display.width * display.height
-                ? `The other model, ${display.model}, has a bigger size of ${display.width * display.height}px² than the current model, ${this.model}, which has a size of ${this.width * this.height}px².`
-                : `Both models, ${this.model} and ${display.model}, have the same size of ${this.width * this.height}px².`
+            (this.width * this.height) > (other.width * other.height)
+                ? `Model: ${this.model} has a larger screen area of ${this.width * this.height}px² than model: ${other.model} with screen area of ${other.width * other.height}px².`
+                : this.width * this.height < other.width * other.height
+                ? `The other model, ${other.model}, has a bigger size of ${other.width * other.height}px² than the current model, ${this.model}, which has a size of ${this.width * this.height}px².`
+                : `Both models, ${this.model} and ${other.model}, have the same size of ${this.width * this.height}px².`
         );
+    }
+
+    // ----->
+    public comparisonPPI(other: Display): void {
+        console.log(
+            this.ppi > other.ppi
+                ? `Model: ${this.model} is sharper (${this.ppi}ppi) than ${other.model} (${other.ppi}ppi)`
+                : this.ppi < other.ppi
+                ? `${other.model} is sharper (${other.ppi}ppi) than ${this.model} (${this.ppi}ppi)`
+                : `${this.model} and ${other.model} are the same as they have the same sharpness (${this.ppi}ppi)`
+        );
+    }
+
+    // -----> 
+    public comparisonMonitor(other: Display): void {
+        this.comparisonArea(other);
+        this.comparisonPPI(other);
     }
 }
 

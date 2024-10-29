5db8d102090a38021b3d09c5d0b485e49ca14fa4
Artiom <artemilico1337@gmail.com>
2024-10-29 17:09:54 +0200

    Completed Task 1

--- laboratory1/Task1/Display.ts
+++ laboratory1/Task1/Display.ts
@@ -19,8 +19,8 @@ export class Display {
             (this.width * this.height) > (other.width * other.height)
                 ? `Model: ${this.model} has a larger screen area of ${this.width * this.height}px² than model: ${other.model} with screen area of ${other.width * other.height}px².`
                 : this.width * this.height < other.width * other.height
-                ? `The other model, ${other.model}, has a bigger size of ${other.width * other.height}px² than the current model, ${this.model}, which has a size of ${this.width * this.height}px².`
-                : `Both models, ${this.model} and ${other.model}, have the same size of ${this.width * this.height}px².`
+                    ? `The model: ${other.model} has a bigger size of ${other.width * other.height}px² than the current model, ${this.model}, which has a size of ${this.width * this.height}px².`
+                    : `Both models, ${this.model} and ${other.model}, have the same size of ${this.width * this.height}px².`
         );
     }
 
@@ -30,8 +30,8 @@ export class Display {
             this.ppi > other.ppi
                 ? `Model: ${this.model} is sharper (${this.ppi}ppi) than ${other.model} (${other.ppi}ppi)`
                 : this.ppi < other.ppi
-                ? `${other.model} is sharper (${other.ppi}ppi) than ${this.model} (${this.ppi}ppi)`
-                : `${this.model} and ${other.model} are the same as they have the same sharpness (${this.ppi}ppi)`
+                    ? `${other.model} is sharper (${other.ppi}ppi) than ${this.model} (${this.ppi}ppi)`
+                    : `${this.model} and ${other.model} are the same as they have the same sharpness (${this.ppi}ppi)`
         );
     }
 
--- laboratory1/Task1/Main.ts
+++ laboratory1/Task1/Main.ts
@@ -2,7 +2,13 @@ import { Display } from "./Display";
 
 class Program {
     static main() {
-     
+
+        let acer = new Display(3840, 2160, 163.18, "Acer Predator X27");
+        let lg = new Display(1920, 1080, 60.00, "LG UltraGear 27GL850");
+        let dell = new Display(2560, 1440, 109.68, "Dell UltraSharp U2720Q");
+        lg.comparisonMonitor(dell);
+        acer.comparisonMonitor(lg);
+        dell.comparisonMonitor(acer);
     }
 }
 

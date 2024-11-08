a323e975bcecd821ddbebd4e83a3e17caf9f5802
cyberArtik <artemilico1337@gmail.com>
2024-11-08 21:25:28 +0200

    modify: PumpkinLatte + SyrypCappuccino with printDetails

--- Task1/Task2/CoffeeClasses/PumpkinSpiceLatte.ts
+++ Task1/Task2/CoffeeClasses/PumpkinSpiceLatte.ts
@@ -1,15 +1,17 @@
-import { Cappuccino, Intensity } from './Cappuccino';
+import { Cappuccino } from "./Cappuccino";
+import { Intensity } from "../EnumCharacteristics/Intensity";
 
 export class PumpkinSpiceLatte extends Cappuccino {
     private mgOfPumpkinSpice: number;
-
-    get MgOfPumpkinSpice(): number {
-        return this.mgOfPumpkinSpice;
-    }
 
     constructor(intensity: Intensity, mlOfMilk: number, mgOfPumpkinSpice: number) {
         super(intensity, mlOfMilk);
         this.coffeeName = "PumpkinSpiceLatte";
         this.mgOfPumpkinSpice = mgOfPumpkinSpice;
+    }
+
+    public printCoffeeDetails(): void {
+        super.printCoffeeDetails();
+        console.log(`Pumpkin Spice Latte pumpkin-spice: ${this.mgOfPumpkinSpice}`);
     }
 }
--- Task1/Task2/CoffeeClasses/SyrupCappuccino.ts
+++ Task1/Task2/CoffeeClasses/SyrupCappuccino.ts
@@ -1,17 +1,18 @@
 import { Cappuccino } from "./Cappuccino";
-import { SyrupType } from "../EnumCharacteristics/SyrupType";
 import { Intensity } from "../EnumCharacteristics/Intensity";
+import { SyrupType } from "../EnumCharacteristics/SyrupType";
 
 export class SyrupCappuccino extends Cappuccino {
     private syrup: SyrupType;
-
-    public get Syrup(): SyrupType {
-        return this.syrup;
-    }
 
     constructor(intensity: Intensity, mlOfMilk: number, syrup: SyrupType) {
         super(intensity, mlOfMilk);
         this.coffeeName = "SyrupCappuccino";
         this.syrup = syrup;
+    }
+
+    public printCoffeeDetails(): void {
+        super.printCoffeeDetails();
+        console.log(`Syrup Cappuccino syrup type: ${SyrupType[this.syrup]}`);
     }
 }

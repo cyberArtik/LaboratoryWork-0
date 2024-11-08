c659c5b32ccf1ea6582e43b19808473896953f08
cyberArtik <artemilico1337@gmail.com>
2024-11-08 21:24:17 +0200

    modify: Americano and Capuccino with printingDetails

--- Task1/Task2/CoffeeClasses/Cappuccino.ts
+++ Task1/Task2/CoffeeClasses/Cappuccino.ts
@@ -1,19 +1,17 @@
-import { Coffee } from './Coffee';
-import { Intensity } from '../EnumCharacteristics/Intensity';
+import { Coffee } from "./Coffee";
+import { Intensity } from "../EnumCharacteristics/Intensity";
 
 export class Cappuccino extends Coffee {
     private mlOfMilk: number;
-
-    get MlOfMilk(): number {
-        return this.mlOfMilk;
-    }
 
     constructor(intensity: Intensity, mlOfMilk: number) {
         super(intensity);
         this.coffeeName = "Cappuccino";
         this.mlOfMilk = mlOfMilk;
     }
+
+    public printCoffeeDetails(): void {
+        super.printCoffeeDetails();
+        console.log(`Cappuccino milk: ${this.mlOfMilk}`);
+    }
 }
-
-export { Intensity };
-
